060417c7fc9f9d4e5c72c6a65c43cc8b1cd9171f
Farid <faridfaracli9@gmail.com>
2024-05-14 00:47:32 +0400

    Haskell file identation made better

--- multiple_square/haskell/main.hs
+++ multiple_square/haskell/main.hs
@@ -4,11 +4,11 @@ mul_square a n = mul_square a (n-1) * mul_square a (n-1)
 
 main :: IO ()
 main = do
-    putStrLn "Real number a:"
-    first <- getLine
-    let a = read first :: Double
-    putStrLn "Positive integer n: "
-    first <- getLine
-    let n = read first :: Int
-    putStr "Result: "
-    print (mul_square a n)
+	putStrLn "Real number a:"
+	first <- getLine
+	let a = read first :: Double
+	putStrLn "Positive integer n: "
+	first <- getLine
+	let n = read first :: Int
+	putStr "Result: "
+	print (mul_square a n)
